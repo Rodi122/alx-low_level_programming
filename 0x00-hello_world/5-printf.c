a3e903e56eef2caac4d992e15f33a9607f8066da
Rodas <getachewrodas39@gmail.com>
2023-08-16 18:13:11 +0300

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Starting point
+ *
+ *Return: 0
+ *
+ *Descripitions: Print the last digit of the number stored in the variable
+ */
+
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
